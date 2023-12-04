a38cf36eeb189d0c938727ea3896f791eeffd246
ChickenLittle02 <115387447+ChickenLittle02@users.noreply.github.com>
2023-12-04 12:47:54 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security;
+
+Console.WriteLine("Hello, World!");
+string codigo = "4+2-3";
+var Prueba = new Tokenizer.Lexer(codigo);
+Prueba.Show_TokenSet();
